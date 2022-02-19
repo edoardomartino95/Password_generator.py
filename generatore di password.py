6c2b5070c818e7d6a8b2321fcaa7d28c8b904160
Edoardo <34552089+Edo-95@users.noreply.github.com>
2022-02-19 12:20:37 +0100

    Add files via upload

---
+++ generatore di password.py
@@ -0,0 +1,30 @@
+import random 
+import time
+
+charArr = [
+'a','b','c','d','e','f','g',
+'h','i','j','k','l','m','n',
+'o','p','q','r','s','t','u',
+'v','w','x','y','z','A','B',
+'C','D','E','F','G','H','I',
+'J','K','L','M','N','O','P',
+'Q','R','S','T','U','V','W',
+'X','Y','Z','0','1','2','3',
+'4','5','6','7','8','9','@',
+'!','|','$','%','&','?','-',
+'_','+','*','#','£','=','€'
+]
+
+def genPsw(arr):
+    long = 16 #lunghezza della password
+    psw = ''
+
+    for i in range(long): 
+        psw += charArr[random.randrange(len(charArr))] #randomizza una lettera per il massimo definito in long
+    
+    return psw
+    
+print(genPsw(charArr))
+    
+
+
