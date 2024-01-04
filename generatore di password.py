387fd9613b8c935a08ca63508cac353c8f26c35a
Edoardo <34552089+edoardomartino95@users.noreply.github.com>
2024-01-04 13:05:57 +0100

    Update generatore di password.py

--- generatore di password.py
+++ generatore di password.py
@@ -1,6 +1,6 @@
 import random 
 
-charArr = [
+myCaratteri = [
 'a','b','c','d','e','f','g',
 'h','i','j','k','l','m','n',
 'o','p','q','r','s','t','u',
@@ -10,53 +10,53 @@ charArr = [
 'Q','R','S','T','U','V','W',
 'X','Y','Z','0','1','2','3',
 '4','5','6','7','8','9','!',
-'£','$','%','&','?','#','@',
-"-", "_", "*", "|", "/", "§"]
+'£','$','%','&','?','#','@']
 
-def randomize_char(arr):
+def randomizzaCaratteri(arr):
     return random.randrange(len(arr)) #randomizza tutti i caratteri dell'array
 
-def gen_psw(arr, lung):
-    my_psw = ''
+def generaPassword(arr, lung):
+    myPassword = ''
 
     for _ in range(lung): 
-        my_psw += arr[randomize_char(arr)] #randomizza una lettera per il massimo definito in len_psw
+        myPassword += arr[randomizzaCaratteri(arr)] #randomizza una lettera per il massimo definito in len_psw
     
-    return my_psw
+    return myPassword
 
 if __name__ == '__main__':
-    
-    piccolaLunghezza = 8
-    mediaLunghezza = 16
-    massimaLunghezza = 32
 
-    print("""
+    def main():
+        run = True
+        while run:
+                piccolaLunghezza = 8
+                mediaLunghezza = 16
+                massimaLunghezza = 32
+
+                print("""
+                        
 GENERATORE PASSWORD:
 Inserisci la lunghezza della password:
-          
+    
 [8] Password ad otto caratteri
 [16] Password a sedici caratteri 
 [32] Password a trentadue caratterri
-[0] Esci dal programma
-""")
+[0] Esci dal programma          
+            """)
 
-while True:
-    lunghezza = int(input("Inserisci la lunghezza: "))
-    if lunghezza == piccolaLunghezza:
-        print("La password scelta di " + str(piccolaLunghezza)+ " caratteri è: ", gen_psw(charArr, lunghezza))
-        break
-    elif lunghezza == mediaLunghezza:
-         print("La password scelta di " + str(mediaLunghezza)+ " caratteri è: ", gen_psw(charArr, lunghezza))
-         break
-    elif lunghezza == massimaLunghezza:
-         print("La password scelta di " + str(massimaLunghezza)+ " caratteri è: ", gen_psw(charArr, lunghezza))
-         break
-    elif lunghezza == 0:
-        
-        esci = input("Per uscire premi S altrimenti premi un pulsante qualsiasi: ")
+                lunghezza = int(input("Inserisci la lunghezza: "))
+                if lunghezza == piccolaLunghezza:
+                    print("La password scelta di " + str(piccolaLunghezza)+ " caratteri è: ", generaPassword(myCaratteri, lunghezza))
+                elif lunghezza == mediaLunghezza:
+                    print("La password scelta di " + str(mediaLunghezza)+ " caratteri è: ", generaPassword(myCaratteri, lunghezza))
+                elif lunghezza == massimaLunghezza:
+                    print("La password scelta di " + str(massimaLunghezza)+ " caratteri è: ", generaPassword(myCaratteri, lunghezza))
+                elif lunghezza == 0:
+                    
+                    esci = input("Per uscire premi S altrimenti premi un pulsante qualsiasi: ")
 
-        if esci.lower() == "s":
-            print("PROGRMMA TERMINATO...")
-            break
-    else:
-        print("Inserisci un valore valido.")
+                    if esci.lower() == "s":
+                        print("PROGRMMA TERMINATO...")
+                        run = False
+                else:
+                    print("Inserisci un valore valido.")
+main()
